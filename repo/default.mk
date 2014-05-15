e8eca7153934ec7a755d9d99232c95f31af19d72
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2014-05-15 19:42:37 +0200

    Add bash completion on repo

--- repo/default.mk
+++ repo/default.mk
@@ -5,6 +5,9 @@
 # Repo url on google storage
 REPO_URL=http://commondatastorage.googleapis.com/git-repo-downloads/repo
 
+# Repo completion script
+REPO_COMPLETION=https://raw.githubusercontent.com/aartamonau/repo.bash_completion/master/repo.bash_completion
+
 # Add repo into the target list
 PHONY += repo
 
@@ -12,8 +15,10 @@ ifdef CONFIG_REPO
 all: repo
 endif
 
-# Shell dependencies if not admin
-ifeq ($(MACHINE_RIGHTS), user)
+# Dependencies
+ifeq ($(MACHINE_RIGHTS), admin)
+repo: bash
+else
 repo: shell
 endif
 
@@ -21,10 +26,12 @@ endif
 repo: git
 	$(PRINT1) INSTALL "$@"
 ifeq ($(MACHINE_RIGHTS), admin)
-	$(PRINT0) WGET "$@"
+	$(PRINT0) WGET "repo"
 	$(QUIET) $(SUDO) $(WGET) /usr/bin/repo $(REPO_URL)
-	$(PRINT0) CHMOD "$@"
+	$(PRINT0) CHMOD "repo"
 	$(QUIET) $(SUDO) chmod 755 /usr/bin/repo
+	$(PRINT0) WGET "repo.bash_completion"
+	$(QUIET) $(SUDO) $(WGET) /etc/bash_completion.d/repo $(REPO_COMPLETION)
 else
 	$(PRINT0) WGET "$@"
 	$(QUIET) $(WGET) $(HOME)/.bin/repo $(REPO_URL)
