1620cbe6c9e63fe8af9452ddda62b1adb0d05156
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2014-05-19 08:04:39 +0200

    Add pkg-config in development tools

--- devtools/default.mk
+++ devtools/default.mk
@@ -15,7 +15,7 @@ ifeq ($(MACHINE_ACCESS), admin)
 	$(PRINT1) INSTALL "$@"
 	$(PRINT0) PACKAGE "$@"
 	$(QUIET) $(S_PACKAGE) install build-essential autoconf automake \
-		libtool libncurses5-dev valgrind gdb
+		libtool libncurses5-dev valgrind gdb pkg-config
 endif
 ifeq ($(MACHINE_TYPE), desktop)
 	$(PRINT0) MKDIR "$(HOME)/development"
