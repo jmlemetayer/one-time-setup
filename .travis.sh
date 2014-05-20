822fde9d327701ef928a651243ed3798232b5908
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2014-05-20 11:06:57 +0200

    Update travis to use the install script

---
+++ .travis.sh
@@ -0,0 +1,18 @@
+sh <(wget -q -O - https://raw.githubusercontent.com/jmlemetayer/one-time-setup/master/install) << EOF
+Travis CI
+travis@travis-ci.com
+server
+admin
+solarized-light
+y
+y
+y
+n
+y
+y
+y
+y
+n
+y
+
+EOF
