bcca46ee58a3c8a6d4e2dab60ef0f451a828c389
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2014-09-10 11:50:46 +0200

    vim: Add HTML5 omnicomplete and syntax

--- vim/default.mk
+++ vim/default.mk
@@ -4,6 +4,7 @@
 
 # Vim colors for solarized theme
 VIM_SOLARIZED := https://github.com/altercation/solarized/raw/master/vim-colors-solarized/colors/solarized.vim
+VIM_HTML5 := https://github.com/othree/html5.vim/archive/master.tar.gz
 
 # Add vim into the target list
 PHONY += vim
@@ -28,6 +29,16 @@ endif
 	$(QUIET) $(MKDIR) $(HOME)/.vim/backup
 	$(PRINT0) COPY "$(HOME)/.vimrc"
 	$(QUIET) $(CP) vim/vimrc $(HOME)/.vimrc
+	$(PRINT0) WGET "html5.vim-master.tar.gz"
+	$(QUIET) $(WGET) /tmp/html5.vim-master.tar.gz $(VIM_HTML5)
+	$(PRINT0) EXTRACT "html5.vim-master.tar.gz"
+	$(QUIET) tar xzf /tmp/html5.vim-master.tar.gz -C /tmp
+	$(PRINT0) INSTALL "html5.vim-master"
+	$(QUIET) $(MAKE) -s -C /tmp/html5.vim-master install >/dev/null 2>/dev/null
+	$(QUIET) rm -rf /tmp/html5.vim-master*
+	$(PRINT0) UPDATE "$(HOME)/.vimrc"
+	$(QUIET) echo >> $(HOME)/.vimrc
+	$(QUIET) cat vim/vimrc.html5 >> $(HOME)/.vimrc
 ifeq ($(CONFIG_THEME), $(filter $(CONFIG_THEME), solarized-dark solarized-light))
 	$(PRINT0) MKDIR "$(HOME)/.vim/colors"
 	$(QUIET) $(MKDIR) $(HOME)/.vim/colors
