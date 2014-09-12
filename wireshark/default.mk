016b0a46ef74bdbd03f6bac6129ef5d3622c19b0
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2014-09-12 09:04:55 +0200

    wireshark: use debconf-utils to install wireshark in the setuid way

--- wireshark/default.mk
+++ wireshark/default.mk
@@ -12,11 +12,12 @@ endif
 endif
 
 # Define the wireshark target
-wireshark: package-update
+wireshark: package-update debconf-utils
 ifeq ($(MACHINE_ACCESS), admin)
 	$(PRINT1) INSTALL "$@"
 	$(PRINT0) PACKAGE "$@"
-	$(QUIET) $(S_PACKAGE) install wireshark
+	$(QUIET) cat wireshark/wireshark.debconf | $(SUDO) debconf-set-selections
+	$(QUIET) $(S_PACKAGE) install wireshark-common wireshark
 endif
 	$(PRINT1) CONFIG "$@"
 	$(PRINT0) MKDIR $(HOME)/.wireshark
@@ -25,7 +26,9 @@ endif
 	$(QUIET) $(CP) wireshark/preferences $(HOME)/.wireshark
 	$(PRINT0) CP colorfilters
 	$(QUIET) $(CP) wireshark/colorfilters $(HOME)/.wireshark
+ifeq ($(MACHINE_ACCESS), admin)
 ifneq ($(shell id -u), 0)
 	$(PRINT0) USERMOD wireshark
 	$(QUIET) $(SUDO) usermod -a -G wireshark $(shell id -un)
+endif
 endif
