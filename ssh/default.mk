b06407ead3651d37c000d53aa2698cec0c75b2bf
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2014-10-23 09:48:39 +0200

    ssh: do not use ssh-agent on server

--- ssh/default.mk
+++ ssh/default.mk
@@ -12,6 +12,15 @@ endif
 # Define the ssh target
 ssh:
 	$(PRINT1) CONFIG "$@"
+ifeq ($(MACHINE_TYPE), server)
+ifeq ($(VERBOSITY), 2)
+	$(QUIET) if [ ! -f $(HOME)/.ssh/id_rsa ]; then \
+		ssh-keygen -t rsa -N "" -f $(HOME)/.ssh/id_rsa; fi
+else
+	$(QUIET) if [ ! -f $(HOME)/.ssh/id_rsa ]; then \
+		ssh-keygen -q -t rsa -N "" -f $(HOME)/.ssh/id_rsa; fi
+endif
+else
 ifeq ($(VERBOSITY), 2)
 	$(QUIET) if [ ! -f $(HOME)/.ssh/id_rsa ]; then \
 		ssh-keygen -t rsa -N "" -f $(HOME)/.ssh/id_rsa; \
@@ -21,4 +30,5 @@ else
 		ssh-keygen -q -t rsa -N "" -f $(HOME)/.ssh/id_rsa; \
 		eval "$(ssh-agent -s)" >/dev/null; \
 		ssh-add ~/.ssh/id_rsa 2>/dev/null; fi
+endif
 endif
