3b860506e80346b243ffb0d24d5098146ab78b51
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2014-11-03 14:39:29 +0100

    devtools: add cmake

--- devtools/default.mk
+++ devtools/default.mk
@@ -15,10 +15,10 @@ ifeq ($(MACHINE_ACCESS), admin)
 	$(PRINT1) INSTALL "$@"
 	$(PRINT0) PACKAGE "$@"
 ifeq ($(MACHINE_TYPE), server)
-	$(QUIET) $(S_PACKAGE) install build-essential autoconf automake \
+	$(QUIET) $(S_PACKAGE) install build-essential cmake autoconf automake \
 		libtool libncurses5-dev valgrind gdb pkg-config
 else
-	$(QUIET) $(S_PACKAGE) install build-essential autoconf automake \
+	$(QUIET) $(S_PACKAGE) install build-essential cmake autoconf automake \
 		libtool libncurses5-dev valgrind gdb pkg-config glogg
 endif
 endif
